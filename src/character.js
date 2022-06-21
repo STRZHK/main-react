0a5f6afc24b1da137ab9c7f133839c589380929b
STRZHK <oleksa.stryzhak@gmail.com>
2022-06-21 19:44:06 +0300

    hw1

---
+++ src/character.js
@@ -0,0 +1,12 @@
+export default function Character(props){
+    console.log(props);
+    let {name, desc, picture} = props;
+
+    return(
+        <div>
+            <h2>{name}</h2>
+            <p>{desc}</p>
+            <img src={picture} alt="bart"/>
+        </div>
+    )
+}
