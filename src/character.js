7edba3dcf73a89e94d351b0deddab6b6bdec23d6
STRZHK <oleksa.stryzhak@gmail.com>
2022-06-22 06:41:37 +0300

    hw1.rick

--- src/character.js
+++ src/character.js
@@ -1,13 +1,32 @@
+// export default function Character(props){
+//     console.log(props);
+//     let {name, desc, picture} = props;
+//
+//     return(
+//
+//         <div>
+//             <h2>{name}</h2>
+//             <p>{desc}</p>
+//             <img src={picture} alt="bart"/>
+//         </div>
+//     )
+// }
+
+
+// SECOND TASK!!!
 export default function Character(props){
     console.log(props);
-    let {name, desc, picture} = props;
+    let {id, name, status, species, gender, image} = props;
 
     return(
 
         <div>
+            <h2>{id}</h2>
             <h2>{name}</h2>
-            <p>{desc}</p>
-            <img src={picture} alt="bart"/>
+            <h2>{status}</h2>
+            <h2>{species}</h2>
+            <p>{gender}</p>
+            <img src={image} alt=""/>
         </div>
     )
 }
